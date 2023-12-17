1fde53cbb48936881d28c47414259dc10b7e3dea
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2023-12-17 14:24:08 -0700

    added metadata object for title and desc

--- src/app/freeforums/layout.tsx
+++ src/app/freeforums/layout.tsx
@@ -1,4 +1,10 @@
 import { Navbar } from "@/components/freeforums";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Free Forums',
+  description: 'A self moderating forum for all your needs.',
+}
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
