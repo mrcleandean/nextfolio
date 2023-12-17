ead910aa8ceb3f84b7dbf6f99f6be1456d74083a
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2023-12-17 14:53:07 -0700

    trying fixed positioning to fix gap

--- src/app/flappybirdclone/page.tsx
+++ src/app/flappybirdclone/page.tsx
@@ -78,7 +78,7 @@ const FlappyBirdClone = () => {
         }
     }, [])
     return (
-        <div className="w-screen h-[100dvh] max-w-screen max-h-[100dvh] flex justify-center items-center">
+        <div className="fixed inset-0 w-screen h-[100dvh] max-w-screen max-h-[100dvh] flex justify-center items-center">
             <canvas
                 ref={canvasRef}
                 width={375}
