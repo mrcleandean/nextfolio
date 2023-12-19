0ee53bd3cb74e5ac9deb7bb8ea9334a00f6f3cce
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2023-12-19 07:04:27 -0700

    added dotenv dep

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,10 @@
+import 'dotenv/config';
 import http from 'http';
 import express from 'express';
 import { setupSocket } from './src/chadchess/index.ts';
 import cors from 'cors';
+
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 const server = http.createServer(app);
@@ -12,5 +15,5 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 8080;
+
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
