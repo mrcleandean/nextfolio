5a545bef8d8d1522a82aae9d322de68e4e60c5ef
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-04-13 12:18:50 -0600

    added paginate-with-boundary demo

---
+++ next/src/app/demos/paginate-with-boundary/page.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const pageinateWithBoundary = (dblist: number[], pageNum: number, pageSize: number, pivotId: number): { result: number[], colors: { [key: number]: string }, pivotIndex: number } => {
+    const result = [];
+    const colors: { [key: number]: string } = {};
+    const pivotIndex = dblist.indexOf(pivotId); // Index to find others from
+    const boundaryIndex = pivotIndex + dblist.length; // If we circle, we cannot go on or past this index
+    const startIndex = pivotIndex + pageNum * pageSize; // pageNum * pageSize is the offset from pivotIndex
+    const endIndex = Math.min(startIndex + pageSize, boundaryIndex); // endIndex cannot go past boundary
+    if (startIndex >= boundaryIndex) return { result: [], colors: {}, pivotIndex }; // If false, we can iterate from start to boundary - 1
+    for (let i = startIndex; i < endIndex; i++) {
+        colors[i % dblist.length] = 'bg-lime-400'
+        result.push(dblist[i % dblist.length]);
+    }
+    return { result, colors, pivotIndex };
+}
+
+const PaginateDemo = () => {
+    const dblist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const [pageNum, setPageNum] = useState(0);
+    const [pageSize, setPageSize] = useState(3);
+    const [pivotId, setPivotId] = useState(0);
+    const [outputArray, setOutputArray] = useState<number[]>([]);
+    const [outputColors, setOutputColors] = useState<{ [key: number]: string }>({});
+    const [pivotI, setPivotI] = useState(0);
+    useEffect(() => {
+        const { result, colors, pivotIndex } = pageinateWithBoundary(dblist, pageNum, pageSize, pivotId);
+        setOutputArray(result);
+        setOutputColors(colors);
+        setPivotI(pivotIndex);
+    }, [pageNum, pageSize, pivotId]);
+    return (
+        <div className="bg-white min-h-screen flex items-center flex-col gap-5">
+            <h1 className="text-black mt-10 text-2xl">Paginate With Boundary</h1>
+            <div className="flex items-center justify-center gap-2">
+                <h1 className="text-black">Dblist</h1>
+                <h1 className="text-black text-3xl">
+                    {'['}
+                    {dblist.map((item, i) => {
+                        return (
+                            <span
+                                key={`dbitem-${i}`}
+                                className={`cursor-pointer select-none decoration-blue-500 ${outputColors[i] ? outputColors[i] : ' bg-white'} ${i === pivotI ? 'underline' : ''}`}
+                                onClick={() => setPivotId(item)}
+                            >
+                                {item}{i === dblist.length - 1 ? '' : ','}
+                            </span>
+                        )
+                    })}
+                    {']'}
+                </h1>
+            </div>
+            <div className="flex items-center justify-center gap-2">
+                <h1 className="text-black">Output</h1>
+                <h1 className="text-black text-3xl">
+                    {`[${outputArray.toString()}]`}
+                </h1>
+            </div>
+            <div className="flex items-center gap-2">
+                <p className="text-black">Page Num: </p>
+                <div onClick={() => {
+                    setPageNum(prev => prev <= 0 ? prev : prev - 1);
+                }} className="bg-violet-300 select-none cursor-pointer w-5 h-5 rounded-lg text-black flex items-center justify-center">-</div>
+                <p className="text-black text-lg select-none">{pageNum}</p>
+                <div onClick={() => {
+                    setPageNum(prev => prev + 1);
+                }} className="bg-violet-300 select-none cursor-pointer w-5 h-5 rounded-lg text-black flex items-center justify-center">+</div>
+            </div>
+            <div className="flex items-center gap-2">
+                <p className="text-black">Page Size: </p>
+                <div onClick={() => {
+                    setPageSize(prev => prev <= 0 ? prev : prev - 1);
+                }} className="bg-violet-300 select-none cursor-pointer w-5 h-5 rounded-lg text-black flex items-center justify-center">-</div>
+                <p className="text-black text-lg select-none">{pageSize}</p>
+                <div onClick={() => {
+                    setPageSize(prev => prev + 1);
+                }} className="bg-violet-300 select-none cursor-pointer w-5 h-5 rounded-lg text-black flex items-center justify-center">+</div>
+            </div>
+            <div className="flex items-center gap-2">
+                <p className="text-black select-none">Pivot Id: {pivotId}</p>
+            </div>
+        </div>
+    )
+}
+
+export default PaginateDemo;
