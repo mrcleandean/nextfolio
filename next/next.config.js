785407eb968d6e56ef59eb718fe086a1583b0c39
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-06 23:23:16 -0600

    readded roomvo demo

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    async redirects() {
+        return [
+            {
+                source: '/demos/views',
+                destination: 'https://m4z62t-5173.csb.app/',
+                permanent: false,
+                basePath: false
+            }
+        ]
+    },
     webpack: (config) => {
         config.module.rules.push({
             test: /\.mp3$/,
--- next/src/app/demos/page.tsx
+++ next/src/app/demos/page.tsx
@@ -13,12 +13,30 @@ const DevDemos = () => {
                 </Link>
                 <h1 className="text-3xl text-black">Dev Demos</h1>
                 <p className="text-black">This page will be built incrementally as required.</p>
-                <Link href="/demos/paginate-with-boundary">
-                    <p className="text-blue-500 underline cursor-pointer">Paginate with boundary</p>
-                </Link>
-                <Link href="/demos/views">
-                    <p className="text-blue-500 underline cursor-pointer">Scalable Views</p>
-                </Link>
+                <ol className="text-black list-decimal ml-5">
+                    <li>
+                        <Link href="/demos/paginate-with-boundary">
+                            <p className="text-blue-500 underline cursor-pointer">Paginate with boundary</p>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/demos/views">
+                            <p className="text-blue-500 underline cursor-pointer">Roomvo views feature</p>
+                        </Link>
+                        <ul className="list-disc list-inside decoration">
+                            <li className="text-black break-all">
+                                <a className="text-blue-500 underline cursor-pointer" href="https://codesandbox.io/p/github/mrcleandean/roomvodemo/main?embed=1&file=%2Fsrc%2FApp.tsx">
+                                    CodeSandbox
+                                </a>
+                            </li>
+                            <li>
+                                <a className="text-blue-500 underline cursor-pointer" href="https://github.com/mrcleandean/roomvodemo">
+                                    GitHub
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ol>
             </div>
         </div>
     )
