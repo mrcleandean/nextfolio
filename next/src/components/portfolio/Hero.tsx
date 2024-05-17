206ad0c722ad745ffecde537060a4cee5bfc4c7c
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-16 19:13:03 -0600

    added lenis scroll to projects button

--- next/src/components/portfolio/Hero.tsx
+++ next/src/components/portfolio/Hero.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { useLenis } from "@studio-freight/react-lenis";
 import { styles, BuddhaCanvas } from "."
 import { motion } from "framer-motion"
 
 const Hero = () => {
+    const lenis = useLenis();
     return (
         <div className="relative w-screen h-screen flex items-center justify-center" id="hero">
             <div className="absolute inset-0 top-20 flex items-center justify-center pointer-events-none touch-none">
@@ -13,21 +15,22 @@ const Hero = () => {
             <div className="absolute inset-0 top-20 z-20 flex items-center justify-center">
                 <BuddhaCanvas />
             </div>
-            <a href="#projects">
-                <div className="absolute bottom-5 rounded-2xl z-40 border-2 w-6 h-14 flex justify-center hover:cursor-pointer">
-                    <motion.div
-                        animate={{
-                            y: [4, 35, 4],
-                        }}
-                        transition={{
-                            duration: 1.5,
-                            repeat: Infinity,
-                            repeatType: 'loop'
-                        }}
-                        className="rounded-full bg-white w-3 h-3"
-                    />
-                </div>
-            </a>
+            <div
+                className="absolute bottom-5 rounded-2xl z-40 border-2 w-6 h-14 flex justify-center hover:cursor-pointer"
+                onClick={() => lenis?.scrollTo('#projects')}
+            >
+                <motion.div
+                    animate={{
+                        y: [4, 35, 4],
+                    }}
+                    transition={{
+                        duration: 1.5,
+                        repeat: Infinity,
+                        repeatType: 'loop'
+                    }}
+                    className="rounded-full bg-white w-3 h-3"
+                />
+            </div>
         </div>
     )
 }
