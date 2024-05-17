c4dce2638bf27e800a9b881d36235ddde4b8ed76
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-17 12:28:46 -0600

    removed unused tailwind extensions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,18 +7,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      animation: {
-        'calc-cube': 'spin 10s infinite',
-      },
       colors: {
         primary: "#11151c",
         secondary: "#83cbe3",
         tertiary: "#344966",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
-        dprimary: 'white',
-        csecondary: '#6380e4'
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
@@ -26,9 +18,6 @@ const config: Config = {
       screens: {
         xs: "450px",
         folionav: "880px"
-      },
-      backgroundImage: {
-        "hero-img": "url('/src/Portfolio/assets/herobg4.png')"
       },
       fontSize: {
         clamp: 'clamp(24px, 24vw, 200px)'
