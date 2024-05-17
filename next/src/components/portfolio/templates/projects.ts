a8ec085580ad4dee5d5220f181f493b46bd371f4
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-17 00:57:58 -0600

    added git public disclaimer

--- next/src/components/portfolio/templates/projects.ts
+++ next/src/components/portfolio/templates/projects.ts
@@ -260,7 +260,7 @@ const projects: ProjectsType[] = [
   },
   {
     name: 'This Portfolio',
-    description: 'This portfolio is a Next.js app that showcases my projects and skills. It is a work in progress and will be updated with new projects and features.',
+    description: 'This portfolio is a Next.js app that showcases my projects and skills. It is a work in progress and will be updated with new projects and features. (GitHub repo going public after heavy ChadChess refactor)',
     tags: [
       {
         name: "Next.js",
@@ -288,7 +288,7 @@ const projects: ProjectsType[] = [
       }
     ],
     image: folio.src,
-    source_code_link: 'https://github.com/mrcleandean/nextfolio',
+    source_code_link: '',
     site_link: "/",
     model: {
       src: '/a.glb',
