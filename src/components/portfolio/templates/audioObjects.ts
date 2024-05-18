659b8a4ca616098f7b4fb0db4f8a67fdc62e838c
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-17 20:22:37 -0600

    removed uncited song

--- src/components/portfolio/templates/audioObjects.ts
+++ src/components/portfolio/templates/audioObjects.ts
@@ -6,6 +6,11 @@ export type AudioObjectType = {
 
 const audioObjects: AudioObjectType[] = [
     {
+        src: '/audio/boneheads.mp3',
+        title: 'Lord Lorenz - Bone Headz',
+        link: 'https://www.youtube.com/watch?v=TZgeu62Cwu8'
+    },
+    {
         src: '/audio/slowedreverb.mp3',
         title: 'cochise - tell em (feat. $not)',
         link: 'https://www.youtube.com/watch?v=SBrpkDZ5Luk&t=0s'
@@ -14,16 +19,6 @@ const audioObjects: AudioObjectType[] = [
         src: '/audio/brokenheart.mp3',
         title: 'Lord Lorenz - BROKEN HEART 4 U',
         link: 'https://www.youtube.com/watch?v=2eMWFHVG7fM'
-    },
-    {
-        src: '/audio/backgroundmusic.mp3',
-        title: 'Unknown - bgmusic',
-        link: '#'
-    },
-    {
-        src: '/audio/boneheads.mp3',
-        title: 'Lord Lorenz - Bone Headz',
-        link: 'https://www.youtube.com/watch?v=TZgeu62Cwu8'
     }
 ];
 
